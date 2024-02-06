b4f860894ffc661ef9dda41a9e28fd0e990980bd
Cristiana Pereira <cristiana.pereira@ideiasdinamicas.com>
2024-02-06 19:06:42 +0000

    transition added.

--- vue-modal-transitions/src/AppModal.vue
+++ vue-modal-transitions/src/AppModal.vue
@@ -9,19 +9,38 @@ defineEmits(["close"]);
 </script>
 
 <template>
-  <div class="modal-wrapper" v-if="show">
-    <div class="modal-wrapper-inner">
-      <button class="modal-close-button" @click="$emit('close')">
-        <XMarkIcon class="w-8 h-8" />
-      </button>
-      <h3 class="modal-title" v-if="title">{{ title }}</h3>
-      <div class="modal-inner">
-        <slot />
+  <div>
+    <Transition>
+      <div class="modal-wrapper" v-if="show">
+        <div class="modal-wrapper-inner">
+          <button class="modal-close-button" @click="$emit('close')">
+            <XMarkIcon class="w-8 h-8" />
+          </button>
+          <h3 class="modal-title" v-if="title">{{ title }}</h3>
+          <div class="modal-inner">
+            <slot />
+          </div>
+        </div>
       </div>
-    </div>
+    </Transition>
   </div>
 </template>
 
 <style scoped>
-/* Provide your transition styles here */
+.v-enter-active,
+.v-leave-active,
+.v-enter-active .modal-wrapper-inner,
+.v-leave-active .modal-wrapper-inner {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter-from .modal-wrapper-inner,
+.v-leave-to .modal-wrapper-inner {
+  transform: translateY(-50px);
+}
 </style>
