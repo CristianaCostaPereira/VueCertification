00a7096d6533d780cde08b6803000d2c05583fd6
Cristiana Pereira <cristiana.pereira@ideiasdinamicas.com>
2024-02-09 19:20:49 +0000

    setingup Vue Router

--- vue-dedicated-movie-pages/src/main.js
+++ vue-dedicated-movie-pages/src/main.js
@@ -1,8 +1,29 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "../style.css";
+import { createRouter, createWebHashHistory } from "vue-router";
+
 
 // import and setup Vue Router here
 // 💡 HINT: You'll also need to import your page components
+import HomePage from "@/pages/HomePage.vue";
+import MovieDetailPage from "@/pages/MovieDetailPage.vue";
 
-createApp(App).mount("#app");
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/:id', component: MovieDetailPage },
+]
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+const app = createApp(App)
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+app.mount('#app')
+
+// createApp(App).mount("#app");
--- vue-dedicated-movie-pages/src/pages/MovieDetailPage.vue
+++ vue-dedicated-movie-pages/src/pages/MovieDetailPage.vue
@@ -5,10 +5,11 @@ import MovieStarRating from "@/MovieStarRating.vue";
 
 // Right now we're just displaying the info for the first movie
 // You should get the id from the URL and display the info for the proper movie
-// 💡 HINT: You do NOT need to modifiy the template
+// 💡 HINT: You do NOT need to modify the template
 
 const movie = ref(items[0]);
 </script>
+
 <template>
   <div
     class="container flex items-center max-w-6xl gap-4 p-10 m-auto dark:text-white"
