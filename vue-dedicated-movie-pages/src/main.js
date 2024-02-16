d00542920fa35e024a68964f8f9d10a748cabb9a
Cristiana Pereira <cristiana.vc.pereira@gmail.com>
2024-02-16 14:52:16 +0000

    code edited in main to keep it more organized an readable

--- vue-dedicated-movie-pages/src/main.js
+++ vue-dedicated-movie-pages/src/main.js
@@ -1,29 +1,28 @@
+// import and setup Vue Router here
+// HINT: You'll also need to import your page components
 import { createApp } from "vue";
 import App from "./App.vue";
 import "../style.css";
 import { createRouter, createWebHistory } from "vue-router";
-
-
-// import and setup Vue Router here
-// 💡 HINT: You'll also need to import your page components
-import HomePage from "@/pages/HomePage.vue";
-import MovieDetailPage from "@/pages/MovieDetailPage.vue";
+import HomePage from "./pages/HomePage.vue";
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/:id', name: 'MovieDetail', component: MovieDetailPage, props: true },
+  { path: '/', name: 'home', component: HomePage },
+  {
+    path: '/:id',
+    name: 'movie-detail',
+    component: () => import("./pages/MovieDetailPage.vue"),
+    props: (route) => ({ id: Number(route.params.id) })
+  },
 ]
 
+const app = createApp(App)
+
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
-  routes, // short for `routes: routes`
+  routes: routes
 })
 
-const app = createApp(App)
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
 app.use(router)
-app.mount('#app')
 
-// createApp(App).mount("#app");
+app.mount("#app");
