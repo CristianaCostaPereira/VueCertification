d561e3c4ac5bb75b9c71f85ec679b07faa7af356
Cristiana Pereira <cristiana.vc.pereira@gmail.com>
2024-02-23 18:41:53 +0000

    Working in style

--- vue-build-a-calculator/src/App.vue
+++ vue-build-a-calculator/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 // Start here your app logic
 </script>
+
 <template>
   <!--	Start here your template -->
-  <div class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
+  <div class="min-w-screen min-h-screen bg-zinc-600 flex items-center justify-center px-5 py-5">
     <div class="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden" style="max-width:300px">
         <div class="w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right">
             <div class="w-full py-5 px-6 text-6xl text-white font-thin">340.0</div>
