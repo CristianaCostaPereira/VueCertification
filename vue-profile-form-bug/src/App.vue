c8e32570f08cf0495c0b17787cea317f16831bc5
Cristiana Pereira <cristiana.vc.pereira@gmail.com>
2024-03-08 11:56:44 +0000

    Working on debugg exercise

--- vue-profile-form-bug/src/App.vue
+++ vue-profile-form-bug/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import CurrentUser from "@/CurrentUser.vue";
 import ProfileForm from "@/ProfileForm.vue";
-import { reactive } from "vue";
+import { ref } from "vue";
 
-let user = reactive({
+let user = ref({
   firstName: "James",
   lastName: "Sullivan",
-  about:
-    "James Sullivan is a renowned scarer at Monsters, Inc. where he works alongside his partner and best friend Mike Wazowski. Sulley's world gets turned upside down when he becomes the accidental guardian of a human child. In working to return Boo to the human world, Sulley manages to uncover a company conspiracy while also solving an energy crisis that plagued the city of Monstropolis.",
+  about: "James Sullivan is a renowned scarer at Monsters, Inc. where he works alongside his partner and best friend Mike Wazowski. Sulley's world gets turned upside down when he becomes the accidental guardian of a human child. In working to return Boo to the human world, Sulley manages to uncover a company conspiracy while also solving an energy crisis that plagued the city of Monstropolis.",
   email: "sulley@monstersinc.com",
   image: "/sully.webp",
 });
@@ -18,7 +17,7 @@ const updateUserData = (data) => {
 </script>
 <template>
   <div class="wrapper">
-    <CurrentUser :person="user" />
+    <CurrentUser :user="user" />
     <ProfileForm :user="user" @update="updateUserData" />
   </div>
 </template>
